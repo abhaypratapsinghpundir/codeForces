e1b458aa8891e20f62740876e793b6054ed952f0
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-09-13 23:04:36 +0530

    281A- word capitalization

---
+++ capitalization.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class capitalization {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+
+        String str= in.next();
+        String result = str.substring(0, 1).toUpperCase() + str.substring(1);
+        System.out.print(result);
+    }
+    
+}
