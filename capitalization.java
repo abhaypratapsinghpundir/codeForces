da88a849f93cb37b4901f631dc354549322fc047
Abhay007 <abhay.pratap.singh.pundir007@gmail.com>
2024-09-13 23:05:09 +0530

    scanne close

--- capitalization.java
+++ capitalization.java
@@ -6,6 +6,7 @@ public class capitalization {
         String str= in.next();
         String result = str.substring(0, 1).toUpperCase() + str.substring(1);
         System.out.print(result);
+        in.close();
     }
     
 }
